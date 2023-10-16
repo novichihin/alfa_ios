ce1abd70162052286405b1f1c9a39b2709cdb79d
novichihin <mr.stepannov@bk.ru>
2023-10-16 19:33:45 +0300

    update code

--- test_ios/ViewController.swift
+++ test_ios/ViewController.swift
@@ -12,8 +12,17 @@ final class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .systemCyan
+        let url: URL = URL(string: "https://api.punkapi.com/v2/beers")!
+        URLSession.shared.dataTask(with: url, completionHandler: {data, responce, error in
+            guard
+                let data = data,
+                let responce = responce,
+                error == nil
+            else { return }
+            let str = String(data: data, encoding: .utf8)
+            print("Получение данных: \(str ?? "")")
+        }).resume()
+        
     }
-
-
 }
 
